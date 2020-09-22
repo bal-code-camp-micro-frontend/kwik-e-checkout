9aa6aa038e89e8e4487da184016e93accdbc7e67
Christian Siegel <christian.siegel@baloise.com>
2020-09-22 11:42:44 +0200

    implement add-to-cart-button

--- public/js/add-to-cart-button.js
+++ public/js/add-to-cart-button.js
@@ -1,6 +1,25 @@
 class AddToCartButton extends HTMLElement {
     connectedCallback() {
-        this.innerHTML = `<h1>Hello from WebComponent1</h1>`;
+        const productId = this.getAttribute("id");
+        this.innerHTML = `
+            <a class="waves-effect waves-teal btn-flat">
+                <i class="material-icons left">add_shopping_cart</i>
+                Add to cart
+            </a>`;
+        this.querySelector("a").addEventListener("click", () => {
+            fetch(`/c/api/product/${productId}`, {method: "PUT"})
+            .then( response => {
+                if (!response.ok) {
+                    alert('Something went wrong:', response);
+                }
+            })
+            .catch(error => {
+                alert('Something went wrong:', error);
+            });
+        });
+    }
+    disconnectedCallback() {
+        this.querySelector("a").removeEventListener("click");
     }
 }
     
