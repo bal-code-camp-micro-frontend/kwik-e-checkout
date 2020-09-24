8e0322e0d98e34a8a1b39bff9bccc840a6a973c3
Christian Siegel <christian.siegel@baloise.com>
2020-09-24 13:55:18 +0200

    use LitElement for add-to-cart-button

--- public/js/add-to-cart-button.js
+++ public/js/add-to-cart-button.js
@@ -1,97 +1,64 @@
+import { LitElement, html } from 'https://unpkg.com/lit-element?module';
 
-const addToCartButtonTemplate = document.createElement('template');
-addToCartButtonTemplate.innerHTML = `
-<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
-<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+export class AddToCartButton extends LitElement {
 
-<link href="/c/css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection" />
-<script src="/c/js/materialize.js"></script>
-
-<a class="waves-effect waves-teal btn-flat green-text">
-    <i class="material-icons left">add_shopping_cart</i>
-    <span>Add to cart</span>
-</a>`;
-
-class AddToCartButton extends HTMLElement {
-    inCart = false
-
-    get productId() {
-        return this.getAttribute('product-id');
-    }
-
-    get filled() {
-        return this.getAttribute('filled') || false;
-    }
-
-    get button() {
-        return this.shadowRoot.querySelector("a");
-    }
-
-    get span() {
-        return this.shadowRoot.querySelector("span");
-    }
-
-    get icon() {
-        return this.shadowRoot.querySelector("i");
-    }
-
-    connectedCallback() {       
-        this.attachShadow({ mode: 'open' });
-        this.shadowRoot.appendChild(addToCartButtonTemplate.content.cloneNode(true));
-        this.buttonCallback = () => this.toggleCart()
-        this.button.addEventListener("click", this.buttonCallback);
-        if (this.filled) {
-            this.button.classList.remove("btn-flat")
-            this.button.classList.add("btn")
-
-            this.button.classList.remove("green-text")
-            this.button.classList.add("green")
-        } 
-
-
-        this.refreshCallback = (e) => {
-            if (e.detail.productId == this.productId) {
-                this.refresh()
-            }
+    static get properties() {
+        return { 
+            productId: {type: String, attribute: 'product-id', },
+            filled: {type: Boolean},
+            inCart: {type: Boolean, attribute: false},
         };
-        window.addEventListener('c:cart:changed', this.refreshCallback);
-
-        this.refresh()
     }
 
-    static get observedAttributes() {return ['product-id']; }
+    constructor() {
+        super();
+        this.filled = false;
+        this.productId = undefined;
+        this.inCart = undefined;
 
-    attributeChangedCallback(attributeName, oldValue, newValue, namespace) {
-        if (attributeName === 'product-id') {
-            this.refresh()
+        window.addEventListener('c:cart:changed', this._cartChangedEventListener);
+        this._reload()
+    }
+
+    render() {
+        const buttonClass = this.filled ? 'btn' : 'btn-flat'
+        const color = (this.inCart !== undefined) ? (this.inCart ? 'red' : 'green') : 'grey'
+        const colorClass = this.filled ? color : `${color}-text`
+        const text = (this.inCart !== undefined) ? (this.inCart ? "Remove from cart" : "Add to cart") : 'Loading...'
+        return html`
+            <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+            <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+            
+            <link href="/c/css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection" />
+            <script src="/c/js/materialize.js"></script>
+            
+            <a @click="${this._toggle}" class="waves-effect waves-teal ${buttonClass} ${colorClass}">
+                <i class="material-icons left">${this.inCart ? "remove_shopping_cart" : "add_shopping_cart"}</i>
+                ${text}
+            </a>
+        `;
+    }
+
+    updated(changedProperties) {
+        if (changedProperties.has('productId')) {
+            this._reload()
         }
     }
 
-    refresh() {
+    disconnectedCallback() {
+        window.removeEventListener('c:cart:changed', this._cartChangedEventListener);
+    }
+
+    _reload() {
         if (this.productId === undefined || this.productId === null) {
             return
         }
         fetch(`/c/api/product/${this.productId}`, {method: "HEAD"})
             .then(response => this.inCart = response.ok)
-            .then(() => {
-                this.span.innerHTML = this.inCart ? "Remove from cart" : "Add to cart"
-                this.icon.innerHTML = this.inCart ? "remove_shopping_cart" : "add_shopping_cart"
-                if (this.filled) {
-                    this.button.classList.remove("green")
-                    this.button.classList.remove("red")
-                    this.button.classList.add(this.inCart ? "red" : "green")
-                } else {
-                    this.button.classList.remove("green-text")
-                    this.button.classList.remove("red-text")
-                    this.button.classList.add(this.inCart ? "red-text" : "green-text")
-                } 
-            })
-            .catch(error => {
-                console.error("oh no:", error)
-            })
+            .catch(error => console.error("oh no:", error))
     }
 
-    toggleCart() {
+    _toggle() {
         fetch(`/c/api/product/${this.productId}`, {method: this.inCart ? "DELETE" : "PUT"})
             .then( response => {
                 if (!response.ok) {
@@ -103,15 +70,14 @@ class AddToCartButton extends HTMLElement {
                     detail: { productId: this.productId }
                 }));
             })
-            .catch(error => {
-                console.error('Something went wrong:', error);
-            });
+            .catch(error => console.error('Something went wrong:', error));
     }
 
-    disconnectedCallback() {
-        this.button.removeEventListener("click", this.buttonCallback);
-        window.removeEventListener('c:cart:changed', this.refreshCallback);
+    _cartChangedEventListener = (e) => {
+        if (e.detail.productId == this.productId) {
+            this._reload()
+        }
     }
 }
-    
+
 customElements.define('c-add-to-cart-button', AddToCartButton);
